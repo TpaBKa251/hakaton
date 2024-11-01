e230f5455cbb7fc1a09871380e6713d368278d87
Лапшин Илья Романович <ilia.lapshin2016@yandex.ru>
2024-11-01 18:54:09 +0700

    feat: добавлен навбар и сайднавбар. Поломана обратно прокрутка

---
+++ src/components/News.js
@@ -0,0 +1,12 @@
+// News.js
+import React from 'react';
+
+function News() {
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+            <h1>Страница новостей в разработке</h1>
+        </div>
+    );
+}
+
+export default News;
---
+++ src/components/Partners.js
@@ -0,0 +1,12 @@
+// Partners.js
+import React from 'react';
+
+function Partners() {
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+            <h1>Страница партнеров в разработке</h1>
+        </div>
+    );
+}
+
+export default Partners;
