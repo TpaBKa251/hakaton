162986d4135d2c4d44ac9179c4c743f530a22ec6
Глеб <gazcoin-1@bk.ru>
2024-11-22 22:29:06 +0700

    fix(hero): Добавлена новая картинка на главной странице.

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './HeroSection.css';
-import kot from '../assets/images/kot (1) (1).png'; // Путь к вашему изображению
-import tpu from '../assets/images/TPU.svg';
-import bh from '../assets/images/BH2_copy.svg';
+
+import kot_big from '../assets/images/Dlya_polozhenia_1 (1) (1).webp'
 
 const HeroSection = () => {
     return (
         <section className="hero">
             <div className="hero-images">
-                <img src={tpu} alt="ТПУ" className="hero-image"/>
-                <img src={kot} alt="Кот" className="hero-image"/>
-                <img src={bh} alt="BH" className="hero-image"/>
+                {/*<img src={tpu} alt="ТПУ" className="hero-image"/>*/}
+                {/*<img src={kot} alt="Кот" className="hero-image"/>*/}
+                {/*<img src={bh} alt="BH" className="hero-image"/>*/}
+                <img src={kot_big} alt="Кот" className="hero-image" />
             </div>
             <div className="hero-content">
                 <h2>Всероссийский предпринимательский хакатон</h2>
